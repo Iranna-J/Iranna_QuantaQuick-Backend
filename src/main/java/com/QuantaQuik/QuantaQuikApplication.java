920504312a8c6971c320cd785d2374fc827e6184
Iranna-J <irannajamdar5170@gmail.com>
2024-07-25 20:23:59 +0530

    initial commit

---
+++ src/main/java/com/QuantaQuik/QuantaQuikApplication.java
@@ -0,0 +1,13 @@
+package com.QuantaQuik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantaQuikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuantaQuikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/QuantaQuik/QuantaQuikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.QuantaQuik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuantaQuikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
